e646a00f47c319207095278ab2e41e0a293d3cb7
SoulCartel117 <jeremiahmartin117@gmail.com>
2021-11-17 14:17:37 -0500

    added slick folder, added carousels to menu page, added css for carousels

---
+++ index.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('.slider1').slick({
+        accessibility: true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        draggable:true,
+        infinte:true,
+        prevArrow: $('.previous1'),
+        nextArrow: $('.next1'),
+    });
+});
+
+$(document).ready(function(){
+    $('.slider2').slick({
+        accessibility: true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        draggable:true,
+        infinte:true,
+        prevArrow: $('.previous2'),
+        nextArrow: $('.next2'),
+    });
+});
+
+$(document).ready(function(){
+    $('.slider3').slick({
+        accessibility: true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        draggable:true,
+        infinte:true,
+        prevArrow: $('.previous3'),
+        nextArrow: $('.next3'),
+    });
+});
+
+$(document).ready(function(){
+    $('.slider4').slick({
+        accessibility: true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        draggable:true,
+        infinte:true,
+        prevArrow: $('.previous4'),
+        nextArrow: $('.next4'),
+    });
+});
